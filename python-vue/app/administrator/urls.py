1d5a976d142745bca1a2a4e0934578cc9dbfe880
Daniel Philip johnson <daniel-philip-johnson@outlook.com>
2021-11-23 19:34:03 +0000

    Created products urls

--- python-vue/app/administrator/urls.py
+++ python-vue/app/administrator/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import AmbassadorAPIView
+from .views import AmbassadorAPIView, ProductGenericAPIView
 
 urlpatterns = [
 
     path('', include('common.urls')),
     path('ambassadors', AmbassadorAPIView.as_view()),
+    path('products', ProductGenericAPIView.as_view()),
 ]

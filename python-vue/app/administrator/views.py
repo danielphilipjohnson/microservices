47f641b952feee77c4829dd0c3f728542107da97
Daniel Philip johnson <daniel-philip-johnson@outlook.com>
2021-11-23 19:33:50 +0000

    Created basic API view
    Decided to set up the cache later

--- python-vue/app/administrator/views.py
+++ python-vue/app/administrator/views.py
@@ -1,10 +1,12 @@
+from rest_framework import generics, mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .serializers import ProductSerializer
 from common.authentication import JWTAuthentication
 from common.serializers import UserSerializer
-from core.models import User
+from core.models import User, Product
 
 class AmbassadorAPIView(APIView):
     authentication_classes = [JWTAuthentication]
@@ -14,3 +16,44 @@ class AmbassadorAPIView(APIView):
         ambassadors = User.objects.filter(is_ambassador=True)
         serializer = UserSerializer(ambassadors, many=True)
         return Response(serializer.data)
+
+class ProductGenericAPIView(
+    generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.ListModelMixin, mixins.CreateModelMixin,
+    mixins.UpdateModelMixin, mixins.DestroyModelMixin
+):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get(self, request, pk=None):
+        if pk:
+            return self.retrieve(request, pk)
+        return self.list(request)
+
+    def post(self, request):
+        response = self.create(request)
+        # for key in cache.keys('*'):
+        #     if 'products_frontend' in key:
+        #         cache.delete(key)
+        # cache.delete('products_backend')
+        return self.create(request)
+        # return response
+
+    def put(self, request, pk=None):
+        response = self.partial_update(request, pk)
+        # for key in cache.keys('*'):
+        #     if 'products_frontend' in key:
+        #         cache.delete(key)
+        # cache.delete('products_backend')
+        return self.partial_update(request,pk)
+        # return response
+
+    def delete(self, request, pk=None):
+        response = self.destroy(request, pk)
+        # for key in cache.keys('*'):
+        #     if 'products_frontend' in key:
+        #         cache.delete(key)
+        # cache.delete('products_backend')
+        return self.delete(request, pk)
+        # return response

ced5bb056820b057f395948de4adf585fa439aac
Daniel Philip johnson <daniel-philip-johnson@outlook.com>
2021-11-23 20:14:08 +0000

    Added endpoints

--- python-vue/app/administrator/serializers.py
+++ python-vue/app/administrator/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework import serializers
 
-from core.models import Product
+from core.models import Product, Link
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
+        fields = '__all__'
+
+
+
+class LinkSerializer(serializers.ModelSerializer):
+    # orders = serializers.SerializerMethodField('get_orders')
+    # def get_orders(self, obj):
+    #     return OrderSerializer(Order.objects.filter(code=obj.code), many=True).data
+
+    class Meta:
+        model = Link
         fields = '__all__'
